27184e8fcc4217e1a2500a0a76909cd77b3c6ea6
eensungkim <eensungkim@gmail.com>
2024-04-05 09:35:55 +0900

    refactor: 탭바컨트롤러 리팩토링
    1. 프로퍼티 -> 메서드 내부로 이동
    2. 탭바아이템 -> Tab 열거형 생성하여 각 케이스별로 관리
    3. 주석 추가

--- BoxOffice/BoxOfficeTabBarController.swift
+++ BoxOffice/BoxOfficeTabBarController.swift
@@ -8,32 +8,55 @@
 import UIKit
 
 final class BoxOfficeTabBarController: UITabBarController {
+    enum Tab: Int {
+        case boxOffice
+        case trailerSearch
+    }
     
-    lazy var boxOfficeNavigationController: UINavigationController = {
-        let boxOfficeNavigationController = UINavigationController(rootViewController: boxOfficeViewController)
-        return boxOfficeNavigationController
-    }()
-    let boxOfficeViewController: BoxOfficeViewController = BoxOfficeViewController()
-    let arSearchTrailerViewController: ARSearchTrailerViewController = ARSearchTrailerViewController()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        setViewControllers([boxOfficeNavigationController, arSearchTrailerViewController], animated: true)
-        setTabBar()
+        
+        setupViewControllers()
+        setupTabBarAppearance()
     }
+}
 
-    func setTabBar() {
-        guard let items = tabBar.items else { return }
-        let boxOfficeItem = items[0]
-        let trailerSearchItem = items[1]
-        
-        boxOfficeItem.title = "박스오피스 순위"
-        boxOfficeItem.image = UIImage(systemName: "chart.line.uptrend.xyaxis")
-        
-        trailerSearchItem.title = "트레일러 검색"
-        trailerSearchItem.image = UIImage(systemName: "camera.metering.center.weighted")
-        
+// MARK: - UI
+extension BoxOfficeTabBarController {
+    private func setupTabBarAppearance() {
         tabBar.backgroundColor = .systemBackground
     }
-    
+}
+
+// MARK: - Configurations
+extension BoxOfficeTabBarController {
+    private func setupViewControllers() {
+        let boxOfficeViewController = BoxOfficeViewController()
+        boxOfficeViewController.tabBarItem = Tab.boxOffice.tabBarItem
+        let boxOfficeNavigationController = UINavigationController(rootViewController: boxOfficeViewController)
+        
+        let arSearchTrailerViewController = ARSearchTrailerViewController()
+        arSearchTrailerViewController.tabBarItem = Tab.trailerSearch.tabBarItem
+        
+        setViewControllers([boxOfficeNavigationController, arSearchTrailerViewController], animated: true)
+    }
+}
+
+extension BoxOfficeTabBarController.Tab {
+    fileprivate var tabBarItem: UITabBarItem {
+        switch self {
+        case .boxOffice:
+            return UITabBarItem(
+                title: "박스오피스 순위",
+                image: UIImage(systemName: "chart.line.uptrend.xyaxis"),
+                tag: self.rawValue
+            )
+        case .trailerSearch:
+            return UITabBarItem(
+                title: "트레일러 검색",
+                image: UIImage(systemName: "camera.metering.center.weighted"),
+                tag: self.rawValue
+            )
+        }
+    }
 }
